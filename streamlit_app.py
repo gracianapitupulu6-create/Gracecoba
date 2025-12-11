b425448b932262d6da01ba5ee83f5980cfc4c20b
gracianapitupulu6-create <gracianapitupulu6@gmail.com>
2025-12-11 15:43:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 number = int(st.number_input("Insert a number",min_value=0, max_value=10))
 if number%2==1:
-  st.write("Bilangan, number, "termasuk bilangan ganjil")
+  st.write("Bilangan", number, "termasuk bilangan ganjil")
 else:
-  st.write("Bilangan, number, "termasuk bilangan genap")
+  st.write("Bilangan", number, "termasuk bilangan genap")
